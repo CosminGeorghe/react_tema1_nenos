6f69b8e6e9769cfb46411359f409153c7126bd47
Gheorghe Cosmin Emilian <cosmin_emilian200@yhoo.com>
2023-04-25 16:43:23 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -16,15 +16,30 @@ function App() {
     "Hello from last component"
   );
 
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
   return (
-    <messageContext.Provider value={lastComponentMessage}>
-       <App2/>;
-    </messageContext.Provider>
+    <>
+      <messageContext.Provider value={lastComponentMessage}>
+        <App2 />;
+      </messageContext.Provider>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
   );
 }
 
 function App2() {
-  return <App3/>;
+  return <App3 />;
 }
 
 function App3() {

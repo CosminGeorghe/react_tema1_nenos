5d02b5df12dab229a33537a80bdd24ecaae9b912
Gheorghe Cosmin Emilian <cosmin_emilian200@yhoo.com>
2023-04-25 18:00:11 +0300

    Revert "useContext, useRef, useCallback"
    This reverts commit 7d8bec29cc4b846313c469bea545064f825ff743.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   useEffect,
   useRef,
   useReducer,
-  useMemo,
+  useMemmo,
   useCallback,
   useContext,
 } from "react";
@@ -12,7 +12,7 @@ import { createContext } from "react";
 const messageContext = createContext();
 
 function App() {
-  const [count2, setCount2] = useState(0);
+
   const [todos, setTodos] = useState([]);
 
   const addTodo = useCallback(() => {
@@ -23,53 +23,25 @@ function App() {
     "Hello from last component"
   );
 
-  const increment = () => {
-    setCount2((c) => c + 1);
-  };
-
-  const expensiveCalculation = (num) => {
-    console.log("Calculating...");
-    for (let i = 0; i < 1000000000; i++) {
-      num += 1;
-    }
-    return num;
-  };
-
-  const calculation = useMemo(() => expensiveCalculation(count2), [count2]);
-
   const [inputValue, setInputValue] = useState("");
-  const count1 = useRef(0);
-
-  const changeInputValue = (value) => {
-    setInputValue(value);
-  };
+  const count = useRef(0);
 
   useEffect(() => {
-    count1.current = count1.current + 1;
+    count.current = count.current + 1;
   });
-
-
 
   return (
     <>
       <messageContext.Provider value={lastComponentMessage}>
         <App2 />;
       </messageContext.Provider>
-      <>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <h1>Render Count: {count1.current}</h1>
-        <Todos todos={todos} addTodo={addTodo} />
-      </>
-      <div>
-        Count: {count2}
-        <button onClick={increment}>+</button>
-        <h2>Expensive Calculation</h2>
-        {calculation}
-      </div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+      <Todos todos={todos} addTodo={addTodo} />
     </>
   );
 }
@@ -99,7 +71,5 @@ const Todos = ({ todos, addTodo }) => {
     </>
   );
 };
-
-
 
 export default App;
